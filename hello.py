86e4d8e123cc4590432b6648c9890836f542b698
cohnj <cohnj@carleton.edu>
2013-11-11 00:50:00 -0600

    ASFDs

--- hello.py
+++ hello.py
@@ -1,8 +1,34 @@
-import os
+from models import *
+from flask import render_template
+from flask import request
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(name)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+
+
+
+conn = Connection()
+
+
+
+@buzz.route('/')
+@buzz.route('/index')
+def index():
+	cities = [c['city'] for c in conn.buzz.locations.find()]
+	kwdata = {}
+	for c in cities:
+		kwdata[c] = [k['text'] for k in conn.buzz.keywords.find({"type" : "c", "city" : c}).sort("count",-1)]
+	return render_template('tweets/index.html',
+		title="buzz",
+		cities = cities,
+		keywords = kwdata)
+		
+@buzz.route('/pie')
+def pie():
+	cities = [c['city'] for c in conn.buzz.locations.find()]
+	kwdata = {}
+	for c in cities:
+		kwdata[c] = [k['text'] for k in conn.buzz.keywords.find({"type" : "c", "city" : c}).sort("count",-1)][:5]	
+	return render_template("pie.html",
+		cities = cities)
