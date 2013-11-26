19641e0ef7b81df2342cc6d5a3fa15e4e95bbc1f
Amanda <amanda.anumba@gmail.com>
2013-11-26 17:53:45 -0600

    CSS

--- app/static/js/buzz.js
+++ app/static/js/buzz.js
@@ -119,7 +119,7 @@ function handleRequest2(request) {
 }
 
 function build(videoId) {
-	var url = "<iframe id='ytplayer' type='text/html' width='260px' height='200px'";
+	var url = "<iframe id='ytplayer' type='text/html' width='275px' height='180px'";
 	url += "src='http://www.youtube.com/embed/"+videoId+"?showinfo=0&controls=0' frameborder='0'/>";
 	return url;
 } 
