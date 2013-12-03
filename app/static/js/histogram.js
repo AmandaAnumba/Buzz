10841e66e83fb7a90250fefb40d198c18918b4f3
Amanda Anumba <amanda.anumba@gmail.com>
2013-12-02 19:28:06 -0600

    Amanda

--- app/static/js/histogram.js
+++ app/static/js/histogram.js
@@ -1,45 +1,19 @@
 // for the histogram that displays on the discover page
 
-$("info").mouseover(function () {
-	var city = document.getElementById("city");
+$(function() {
+	//$.getJSON( "static/json/"+city+"_media_values.json", function( data ) {
+	var city = document.getElementById("city").innerHTML;
+	var sampledata = {
+		"Rihanna": "85",
+		"Macklemore": "75",
+		"One Direction": "58",
+		"Skrillex": "36",
+		"Avicii": "29"
+	};
 
-	$.getJSON( "static/json/"+city+"_media_values.json", function( data ) {
-
-		var container = document.getElementById("histchart");
-
-		$.each( data, function( i, top_keywords ) {
-		  	var list = document.createElement("ul");
-		    list.className = "terms";
-		    container.appendChild(list);
-
-		    for (i==0; i<5; i++) {
-		    	var list-inner = document.createElement("li");
-				list.appendChild(list-inner);
-				
-				var span = document.createElement("span");
-				span.className = "pre";
-				list-inner.appendChild(span);
-
-				// calculate the count here
-				// count = 
-				// it effects the width of the histogram
-				span.innerHTML = "<div class='count'>"+count+"</div>";
-
-				var bar-wrap = document.createElement("div");
-				bar-wrap.className = "inner";
-				list-inner.appendChild(bar-wrap);
-
-				var bar = document.createElement("div");
-				bar.className = "bar";
-				// width = 
-				bar.setAttribute("width", width);
-				// calculate the width here, in percents 
-				// ex: count for top trending topic divided by count 
-				// for second top trending top * 100
-				bar.innerHTML = '<p class="text2">' + keyword.label + '</p>';
-				inner.appendChild(bar);
-
-		  	}		
-		});
+	$.each(sampledata, function(key, val) {
+		$('.terms').append("<li><div class='inner'><span class='pre'><div class='count'>"+val+"</div></span><div class='bar' style='width:"+val+"%'><p class='text2'>" + key + "</p></div></div></li>");
 	}); 
+
+	$("<br/>").insertAfter('.bar');
 });
