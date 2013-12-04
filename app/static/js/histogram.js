1fa90c7672d797e16c61038b7068613fd8303015
Amanda Anumba <amanda.anumba@gmail.com>
2013-12-03 21:19:48 -0600

    Amanda

--- app/static/js/histogram.js
+++ app/static/js/histogram.js
@@ -2,35 +2,33 @@
 
 $(function() {
 
-	//console.log(citieslist)
 	citieslist.forEach(function(city) {
-		//console.log(city)
-	//var city = document.getElementById("city").innerHTML;
-	$.getJSON( "static/json/" + city + "_media_values.json", function( data ) {
-		//console.log(data)
-	//var sampledata = {};
-	$('#terms' + city).append("<li><div class='inner'><span class='pre'>");
-	$.each( data, function( i, keyword ) {
-		//console.log(keyword)
-        if ( i === 5 ) {
-        $('.terms' + city.replace(/ /g,"_")).append("</div></li>");
-        
-        console.log($('.terms' + city.replace(/ /g,"_")))
-       // $("<br/>").insertAfter('.bar' + city);
-        return false;
-    }
-    	else {
-    		valadd = keyword.value * 20;
-    		val = 20 + valadd;
-    		key = keyword.label;
-    		$('.terms' + city.replace(/ /g,"_")).append("<span class='pre'><div class='count'>"+(i + 1)+"</div></span><div class='bar'" + city.replace(/ /g,"_") + " style='width:"+val+"%'><p class='text2'>" + key + "</p></div><br/><br/>");
-    		
-    	}
 
-      });
-	$("<br/>").insertAfter('.bar' + city.replace(/ /g,"_"));
-});
-});
+		$.getJSON( "static/json/" + city + "_media_values.json", function( data ) {
+
+			$('#terms' + city).append("<li><div class='inner'><span class='pre'>");
+			
+			$.each( data, function( i, keyword ) {
+				//console.log(keyword)
+		        if ( i === 5 ) {
+			        $('#terms' + city.replace(/ /g,"_")).append("</div></li>");
+			        
+			        //console.log($('#terms' + city.replace(/ /g,"_")))
+			       // $("<br/>").insertAfter('.bar' + city);
+			        return false;
+		    	}
+		    	
+		    	else {
+		    		valadd = keyword.value * 20;
+		    		val = 20 + valadd;
+		    		key = keyword.label;
+		    		$('#terms' + city.replace(/ /g,"_")).append("<span class='pre'><div class='count'>"+(i + 1)+"</div></span><div class='bar'" + city.replace(/ /g,"_") + " style='width:"+val+"%'><p class='text2'>" + key + "</p></div><br/><br/>");
+		    	}
+		    });
+			
+			$("<br/>").insertAfter('.bar' + city.replace(/ /g,"_"));
+		});
+	});
 });
 	/*var sampledata = {
 		"Rihanna": "85",
